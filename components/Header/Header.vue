c911bb253d2e22afce3939b2e598935232940925
pratama.rexy <pratamarexy93@gmail.com>
2019-03-16 17:30:55 +0700

    first init

---
+++ components/Header/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="app-header navbar">
+    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <b-link class="navbar-brand" to="#">
+      <div class="user-avatar"><img src="/img/pp.png"></div>
+      <div class=user-detail>
+        <h4>John Doe </h4>
+        <p>Online</p>
+      </div>
+    </b-link>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" @click="sidebarToggle">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item class="d-md-down-none">
+        <img src="/img/ic-bell.svg">
+        <b-badge pill variant="danger">5</b-badge>
+      </b-nav-item>
+      <b-nav-item class="d-md-down-none">
+        <img src="/img/ic-setting.svg">
+      </b-nav-item>
+      <b-nav-item class="d-md-down-none">
+        <img src="/img/ic-help.svg">
+      </b-nav-item>
+    </b-navbar-nav>
+  </header>
+</template>
+
+<script>
+  import HeaderDropdown from './HeaderDropdown.vue'
+
+  export default {
+    name: 'c-header',
+    components: {
+      HeaderDropdown
+    },
+    methods: {
+      sidebarToggle (e) {
+        e.preventDefault()
+        document.body.classList.toggle('sidebar-hidden')
+      },
+      sidebarMinimize (e) {
+        e.preventDefault()
+        document.body.classList.toggle('sidebar-minimized')
+      },
+      mobileSidebarToggle (e) {
+        e.preventDefault()
+        document.body.classList.toggle('sidebar-mobile-show')
+      },
+      asideToggle (e) {
+        e.preventDefault()
+        document.body.classList.toggle('aside-menu-hidden')
+      }
+    }
+  }
+</script>
---
+++ layouts/menu.js
@@ -0,0 +1,53 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: 'Menu Utama',
+      class: '',
+      wrapper: {
+        element: '',
+        attributes: {}
+      }
+    },
+    {
+      name: 'Dashboard',
+      url: '/',
+      icon: '/img/ic_dashboard.png',
+      // badge: {
+      //   variant: 'primary',
+      //   text: 'NEW'
+      // }
+    },
+    
+    {
+      name: 'Data Pegawai',
+      url: '/',
+      icon: '/img/ic_data_pegawai.png'
+    },
+    {
+      name: 'Data Wilayah',
+      url: '/',
+      icon: '/img/ic_wilayah.png'
+    },
+    {
+      name: 'Data Pelanggan',
+      url: '/',
+      icon: '/img/ic_pelanggan.png'
+    },
+    {
+      name: 'Data Summary',
+      url: '/',
+      icon: '/img/ic_summary.png'
+    },
+    {
+      name: 'Work Order',
+      url: '/',
+      icon: '/img/ic_wo.png'
+    },
+    {
+      name: 'Reporting',
+      url: '/',
+      icon: '/img/ic_report.png'
+    }
+  ]
+}

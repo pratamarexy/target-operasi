8112fc98d79c73773c1da905eb4b87fa8376594e
pratama.rexy <pratamarexy93@gmail.com>
2019-03-16 17:36:52 +0700

    edit footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
   <footer class="app-footer">
-    <span><a href="http://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-    <span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
+    <span><a href="#">Target Operasi</a> &copy; 2019.</span>
   </footer>
 </template>
 <script>
